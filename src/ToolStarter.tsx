89fa8817e93cac8978c2bd617c284f661f38001b
idcs <idc.dts@gmail.com>
2022-06-17 12:44:28 +0100

    Extension will now display an ordered list of valid tools

--- src/ToolStarter.tsx
+++ src/ToolStarter.tsx
@@ -3,13 +3,14 @@ import * as path from 'path';
 import * as React from 'react';
 import { Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-import { fs, selectors, Spinner, types, util } from 'vortex-api';
-import ToolIcon from './ToolIcon';
+import { fs, selectors, Spinner, ToolIcon, types, util } from 'vortex-api';
 
 // tslint:disable-next-line:no-var-requires
 const { MainContext } = require('vortex-api');
 
 interface IConnectedProps {
+  addToTitleBar: boolean;
+  toolsOrder: types.IStarterInfo[];
   game: types.IGameStored;
   discovery: types.IDiscoveryResult;
   discoveredTools: { [id: string]: types.IDiscoveredTool };
@@ -17,17 +18,21 @@ interface IConnectedProps {
   toolsRunning: { [exeId: string]: types.IRunningTool };
 }
 
-type IProps = IConnectedProps;
+interface IToolStarterIconProps {
+  tool: types.IStarterInfo;
+  running: boolean;
+  iconLocation: string;
+}
 
 function toolIconRW(gameId: string, toolId: string) {
   return path.join((util as any).getVortexPath('userData'), gameId, 'icons', toolId + '.png');
 }
 
-function toolIcon(gameId: string, extensionPath: string,
-                  toolId: string, toolLogo: string): string {
+async function toolIcon(gameId: string, extensionPath: string,
+                        toolId: string, toolLogo: string): Promise<string> {
   try {
     const iconPath = toolIconRW(gameId, toolId);
-    fs.statSync(iconPath);
+    await fs.statAsync(iconPath);
     return iconPath;
   } catch (err) {
     if (toolLogo === undefined) {
@@ -35,7 +40,7 @@ function toolIcon(gameId: string, extensionPath: string,
     }
     try {
       const iconPath = path.join(extensionPath, toolLogo);
-      fs.statSync(iconPath);
+      await fs.statAsync(iconPath);
       return iconPath;
     } catch (err) {
       return undefined;
@@ -54,14 +59,8 @@ function toStarterInfo(game: types.IGameStored,
 function starterMemoizer(game: types.IGameStored,
                          discovery: types.IDiscoveryResult,
                          primaryTool: string,
-                         tools: types.IDiscoveredTool[]) {
-  return tools
-    .filter(tool => tool.id !== primaryTool)
-    .sort((lhs: any, rhs: any) => {
-      const tlhs = lhs.timestamp ?? 0;
-      const trhs = rhs.timestamp ?? 0;
-      return tlhs - trhs;
-    })
+                         tools: types.IDiscoveredTool[]): types.IStarterInfo[] {
+  return tools.filter(tool => tool.id !== undefined)
     .map(toolDiscovery => {
       const tool = game.supportedTools.find(iter => iter.id === toolDiscovery.id);
       try {
@@ -74,27 +73,40 @@ function starterMemoizer(game: types.IGameStored,
     .filter(iter => iter !== undefined);
 }
 
-function ToolStarterIcon(props: { tool: types.IStarterInfo, running: boolean }) {
+function ToolStarterIcon(props: IToolStarterIconProps) {
   const { api }: { api: types.IExtensionApi } = React.useContext(MainContext);
+  const { primaryTool } = useSelector(mapStateToProps);
   const onShowError = React.useCallback((message: string, details: any, allowReport: boolean) => {
     api.showErrorNotification(message, details, { allowReport });
   }, [api]);
+  const [valid, setValid] = React.useState(false);
+  React.useEffect(() => {
+    const isValid = async () => {
+      try {
+        await fs.statAsync(props.tool.exePath);
+        setValid(true);
+      } catch (err) {
+        setValid(false);
+      }
+    };
+    isValid();
+  }, [props.tool]);
 
   const startCB = React.useCallback(() => {
     util.StarterInfo.run(props.tool as any, api, onShowError);
   }, [props]);
 
-  return (
-    <Button
-      className='titlebar-button'
-      title={props.tool.name}
-      onClick={startCB}
-      disabled={props.running}
+  return valid ? (
+    <ToolIcon
+      t={api.translate}
+      valid={true}
+      isPrimary={props.tool.id === primaryTool}
+      imageUrl={props.iconLocation}
+      onRun={startCB}
     >
-      <ToolIcon imageUrl={`${props.tool.iconOutPath}?${(props.tool as any).timestamp || 0}`} altUrl={props.tool.iconPath} />
       {props.running ? <Spinner className='running-overlay' /> : null}
-    </Button>
-  );
+    </ToolIcon>
+  ) : null;
 }
 
 const toStarters = memoize(starterMemoizer);
@@ -104,18 +116,44 @@ function makeExeId(exePath: string): string {
 }
 
 function ToolStarter() {
-  const { discovery, game, discoveredTools,
-          primaryTool, toolsRunning } = useSelector(mapStateToProps);
+  const { addToTitleBar, discovery, game, discoveredTools,
+          primaryTool, toolsRunning, toolsOrder } = useSelector(mapStateToProps);
 
+  const [toolImages, setToolImages] = React.useState({});
   const starters = toStarters(game, discovery, primaryTool, Object.values(discoveredTools || {}));
-
+  const idxOfTool = (tool) => {
+    const idx = toolsOrder.findIndex(id => tool.id === id);
+    return idx !== -1 ? idx : starters.length;
+  };
+  starters.sort((lhs, rhs) => idxOfTool(lhs) - idxOfTool(rhs));
+  React.useEffect(() => {
+    const getImagePath = async () => {
+      const imageMap = {};
+      for (const starter of starters) {
+        imageMap[starter.id] = await toolIcon(game.id, game.extensionPath,
+                                              starter.id, starter.logoName);
+      }
+      setToolImages(imageMap);
+    };
+    getImagePath();
+  }, [toolImages]);
+  if (!addToTitleBar) {
+    return null;
+  }
   return (
     <div id='titlebar-starter'>
       {starters.map((starter, idx) => {
         const running = (starter.exePath !== undefined)
           && (toolsRunning[makeExeId(starter.exePath)] !== undefined);
 
-        return <ToolStarterIcon running={running} key={idx} tool={starter} />;
+        return (
+          <ToolStarterIcon
+            running={running}
+            key={idx}
+            tool={starter}
+            iconLocation={toolImages[starter.id]}
+          />
+        );
       })}
     </div>
   );
@@ -128,6 +166,10 @@ function mapStateToProps(state: types.IState): IConnectedProps {
   const discovery: types.IDiscoveryResult = selectors.currentGameDiscovery(state);
 
   return {
+    addToTitleBar: util.getSafe(state,
+      ['settings', 'interface', 'tools', 'addToolsToTitleBar', game.id], false),
+    toolsOrder: util.getSafe(state,
+      ['settings', 'interface', 'tools', 'order', game.id], []),
     game,
     discovery,
     discoveredTools: game !== undefined
