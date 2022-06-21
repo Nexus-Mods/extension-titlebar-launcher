a57c0f815d1f20ceb4946617ca46f4c7968f1e0a
idcs <idc.dts@gmail.com>
2022-06-21 11:18:15 +0100

    code review changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,20 @@ import * as path from 'path';
 import * as React from 'react';
 import { selectors, types } from 'vortex-api';
 
+import settingsReducer from './reducers';
+
 import ToolStarter from './ToolStarter';
+import TitleBarToggle from './TitlebarToggle';
 
 function init(context: types.IExtensionContext) {
+  context.registerReducer(['settings', 'interface'], settingsReducer);
   (context as any).registerDynDiv('main-toolbar', ToolStarter, {
+    condition: (props: any): boolean => {
+      return selectors.activeGameId(context.api.store.getState()) !== undefined;
+    },
+  });
+
+  (context as any).registerDynDiv('starter-dashlet-tools-controls', TitleBarToggle, {
     condition: (props: any): boolean => {
       return selectors.activeGameId(context.api.store.getState()) !== undefined;
     },
