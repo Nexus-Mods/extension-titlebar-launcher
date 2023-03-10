6c4a96ef2c6758e9d7b08c77bbbb7af44b93689f
TanninOne <seppleviathan@gmx.de>
2023-03-10 10:03:00 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('titlebar-launcher', __dirname, 4);
+module.exports = webpack('titlebar-launcher', __dirname, 5);
